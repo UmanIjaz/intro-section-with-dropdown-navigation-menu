337a24be7e911f5122cc92d0222b118d855f998c
umanijaz5 <umanijaz5@gmail.com>
2024-02-04 10:29:16 +0500

    crate Nav logo with links

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+    },
+    colors: {
+      c_white: "hsl(0, 0%, 98%)",
+      c_gray: "hsl(0, 0%, 41%)",
+      c_black: "hsl(0, 0%, 8%)"
+    },
+  },
+    
   },
   plugins: [],
 }
